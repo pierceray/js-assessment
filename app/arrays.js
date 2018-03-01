095582c8a0551277f70fbc71db6f514da355d564
Ray Pierce <rpierce@rmn.com>
2018-03-01 09:33:28 -0600

    implemented indexOf, remove, removeWithoutCopy, append, truncate, prepend, curtail, concat, insert, count

--- app/arrays.js
+++ app/arrays.js
@@ -2,7 +2,13 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        return i;
+      }
+    }
 
+    return -1;
   },
 
   sum: function(arr) {
@@ -16,39 +22,63 @@ exports.arraysAnswers = {
   },
 
   remove: function(arr, item) {
-
+    var newArray = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] !== item) {
+        newArray.push(arr[i]);
+      }
+    }
+    return newArray;
   },
 
   removeWithoutCopy: function(arr, item) {
-
+    for (var i = arr.length - 1; i >= 0; i--) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
   },
 
   append: function(arr, item) {
-
+    var newArray = arr;
+    newArray.push(item);
+    return newArray;
   },
 
   truncate: function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail: function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat: function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert: function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count: function(arr, item) {
+    var count = 0;
+    for(var i = 0; i < arr.length; i++) {
+      if (arr[i] === item){
+        count++;
+      }
+    }
 
+    return count;
   },
 
   duplicates: function(arr) {
